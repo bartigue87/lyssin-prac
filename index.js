d97bf7c64ed3a72efb4194ae53486cd3b8971ab9
Brandon Artigue <bartigue87@yahoo.com>
2021-07-19 23:46:02 -0600

    initial commit

---
+++ index.js
@@ -0,0 +1,18 @@
+const container = document.querySelector('.container');
+
+fetch('./people.json')
+  .then((response) => {
+    return response.json();
+  })
+  .then((data) => {
+    for (let i = 0; i < data.length; i++) {
+      let div = document.createElement('div');
+      div.textContent = `${
+        data[i].lname[0].toUpperCase() + data[i].lname.slice(1)
+      }, ${data[i].fname[0].toUpperCase() + data[i].fname.slice(1)}`;
+
+      div.classList.add('name');
+      container.appendChild(div);
+      console.log(div.textContent);
+    }
+  });
